38699804b60f911d050e6751169970c08be85447
Scott McKell <scott.g.mckell@gmail.com>
2018-05-23 20:46:12 -0600

    add flikr api calls

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,21 @@ class StaticPagesController < ApplicationController
 
 require 'flickraw'
 
+FlickRaw.api_key = ENV["API_KEY"]
+FlickRaw.shared_secret = ENV["SHARED_SECRET"]
+
 def index
-	@photos = Photo.all
+	begin
+	if params[:/][:id]
+		@photos = []
+		@photoset = flickr.photos.search(user_id: params[:/][:id])
+		@photoset.each do |ph|
+			@photos << FlickRaw.url(ph)
+		end
+	end
+	rescue
+	end
 end
+
 
 end
